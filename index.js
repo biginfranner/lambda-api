a224f55f04d763b8a1e7e291fb419820423f8fa2
biginfranner <11asdfjkl22@gmail.com>
2026-01-31 17:27:10 +0900

    add SAM template

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
-const message = "Hello Lambda";
-console.log(message);
+const {
+  DynamoDBClient,
+  PutItemCommand,
+  ScanCommand,
+} = require("@aws-sdk/client-dynamodb");
+
+const client = new DynamoDBClient({ region: "ap-northeast-1" });
+const TABLE_NAME = process.env.MEMO_TABLE_NAME;
+
+exports.handler = async (event) => {
+  console.log("Received event:", JSON.stringify(event));
+
+  try {
+    const body = event.body ? JSON.parse(event.body) : {};
+
+    if (event.httpMethod === "POST") {
+      const params = {
+        TableName: TABLE_NAME,
+        Item: {
+          id: { S: Date.now().toString() },
+          text: { S: body.text || "" },
+        },
+      };
+
+      await client.send(new PutItemCommand(params));
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Added" }),
+      };
+    }
+
+    if (event.httpMethod === "GET") {
+      const data = await client.send(
+        new ScanCommand({
+          TableName: TABLE_NAME,
+        }),
+      );
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data.Items),
+      };
+    }
+
+    return {
+      statusCode: 400,
+      body: "Unsupported method",
+    };
+  } catch (err) {
+    console.error("Error in handler:", err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Error" }),
+    };
+  }
+};
